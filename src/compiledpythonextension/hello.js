eef4df2c95d0f8522c3ca5cc5d8c8712f61c7c24
Maya Shankar <mshanka@ncsu.edu>
2016-12-15 23:25:39 +0400

    tutorial doesn't work???

--- src/compiledpythonextension/hello.js
+++
@@ -1,8 +0,0 @@
-
-var __name__ = "__main__";
-function doHelloMessage() {
-    alert("hello");
-}
-doHelloMessage();
-document.getElementById("result").innerHTML = "Compiled Python script in Chrome";
-console.log("hello from python");
--- src/make.sh
+++
@@ -1,4 +0,0 @@
-rapydscript -b -m -p hello.py > hello.js
-rm -rf compiledpythonextension/hello.js
-mkdir compiledpythonextension
-mv hello.js compiledpythonextension
